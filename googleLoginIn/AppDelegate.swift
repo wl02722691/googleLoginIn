1ba7480b8c928d620e95dec778a3b2059649d80b
“Alice” <wl02722691@gmail.com>
2018-02-10 21:16:45 +0800

    google install

--- googleLoginIn/AppDelegate.swift
+++ googleLoginIn/AppDelegate.swift
@@ -8,16 +8,65 @@
 
 import UIKit
 import CoreData
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    
 
     var window: UIWindow?
 
 
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error)
+            return
+        }
+        guard let authentication = user.authentication else {return}
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            let vc = self.window?.rootViewController as! ViewController
+            vc.emailLabel.text = user?.email
+            vc.nameLabel.text = user?.displayName
+            
+            DispatchQueue.global().async {
+                let imgData = try? Data(contentsOf: (user?.photoURL)!)
+                
+                DispatchQueue.main.async {
+                    if let data = imgData{
+                        let image = UIImage(data: data)
+                        vc.profileImgView.image = image
+                    }
+                }
+            }
+        
+        }
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+        -> Bool {
+            return GIDSignIn.sharedInstance().handle(url,
+                                                     sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                     annotation: [:])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- googleLoginIn/ViewController.swift
+++ googleLoginIn/ViewController.swift
@@ -6,13 +6,27 @@
 //  Copyright © 2018年 AliceChang. All rights reserved.
 //
 
+
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,GIDSignInUIDelegate {
+    @IBOutlet weak var profileImgView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBAction func login(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signInSilently()
+        //signInSilently()只要登入過一次就可以快速登入
     }
 
     override func didReceiveMemoryWarning() {
